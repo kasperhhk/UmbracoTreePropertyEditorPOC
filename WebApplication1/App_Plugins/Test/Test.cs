6716787a872bb0fbcaae4b4d5db08e3e46dc8f81
kasperhhk <kkr@dis-play.dk>
2016-05-14 02:13:29 +0200

    fix + add

--- WebApplication1/App_Plugins/Test/Test.cs
+++ WebApplication1/App_Plugins/Test/Test.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Umbraco.Core.PropertyEditors;
+﻿using Umbraco.Core.PropertyEditors;
 
 namespace WebApplication1.App_Plugins.Test
 {
     [PropertyEditor("NCTest", "NCTest", "/App_Plugins/Test/test.html", ValueType = "JSON")]
-    public class Test
+    public class Test : PropertyEditor
     {
+        protected override PreValueEditor CreatePreValueEditor()
+        {
+            return new Prevalue();
+        }
+
         public class Prevalue : PreValueEditor
         {
             [PreValueField("NCTestPrevalue", "Datatype", "/App_Plugins/Test/test.datatype.html", Description = "Select datatype")]
